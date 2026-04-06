b772829ea92986937e6e5c231e8cd49ad06b420b
Renan Emanoel <renanemanoel33@gmail.com>
2026-04-06 22:36:40 +0000

    frase levemente modificada

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return<h1>A vida das pessoas não acabam quando elas morrem, mas sim quando perdem a fé.</h1>
+    return<h1>A vida das pessoas não acabam quando elas morrem, mas sim quando perdem a fé...</h1>
 }
 
 export default Home;
