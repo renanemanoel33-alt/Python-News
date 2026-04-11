7dd6d28e447e497437854577f9d138b7f86be54a
Renan Emanoel <renanemanoel33@gmail.com>
2026-04-11 00:45:38 +0000

    adicionando scripts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Python-News
+
 meu projeto inicial
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return<h1>A vida das pessoas não acabam quando elas morrem, mas sim quando perdem a fé...</h1>
+  return (
+    <h1>
+      A vida das pessoas não acabam quando elas morrem, mas sim quando perdem a
+      fé...
+    </h1>
+  );
 }
 
 export default Home;
